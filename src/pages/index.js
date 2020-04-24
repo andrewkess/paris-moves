f2b94b30d7c240db3ae9b5b0669d675b1981aa43
Andrew Kessinger <53582334+andrewkess@users.noreply.github.com>
2020-04-25 00:10:22 +0200

    adding in new index code

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,84 @@ class RootIndex extends React.Component {
 
 
 
+
+    function format(amount, currency) {
+      return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency,
+      }).format((amount / 100).toFixed(2));
+    }
+
+    async function handleSubmit(event) {
+      event.preventDefault();
+      const form = new FormData(event.target);
+
+      const data = {
+        sku: form.get('sku'),
+        quantity: Number(form.get('quantity')),
+      };
+
+      const response = await fetch('/.netlify/functions/create-checkout', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      }).then((res) => res.json());
+
+      const stripe = Stripe(response.publishableKey);
+      const { error } = await stripe.redirectToCheckout({
+        sessionId: response.sessionId,
+      });
+
+      if (error) {
+        console.error(error);
+      }
+    }
+
+    async function loadProducts() {
+      if (!'content' in document.createElement('template')) {
+        console.error('Your browser doesn’t support HTML template elements.');
+        return;
+      }
+
+      const data = await fetch('/.netlify/functions/get-products')
+        .then((res) => res.json())
+        .catch((err) => console.error(err));
+
+      const products = document.querySelector('.products');
+      const template = document.querySelector('#product');
+
+      data.forEach((product) => {
+        const container = template.content.cloneNode(true);
+
+        container.querySelector('h2').innerText = product.name;
+        container.querySelector('.description').innerText =
+          product.description;
+        container.querySelector('.price').innerText = format(
+          product.amount,
+          product.currency
+        );
+        container.querySelector('[name=sku]').value = product.sku;
+
+        const img = container.querySelector('img');
+        img.src = product.image;
+        img.alt = product.name;
+
+        const form = container.querySelector('form');
+        form.addEventListener('submit', handleSubmit);
+
+        products.appendChild(container);
+      });
+    }
+
+    loadProducts();
+
+    
+
+
+
+
     return (
       <Layout location={this.props.location}>
         <div style={{ background: '#fff' }}>
@@ -32,6 +110,31 @@ class RootIndex extends React.Component {
             </ul>
           </div>
         </div>
+
+        <template id="product">
+      <div className="product">
+        <img src="" alt="" />
+        <h2>name</h2>
+        <p className="description">description</p>
+        <p className="price">price</p>
+        <form action="/.netlify/functions/create-checkout" method="post">
+          <label for="quantity">Quantity</label>
+          <input
+            type="number"
+            id="quantity"
+            name="quantity"
+            value="1"
+            min="1"
+            max="10"
+          />
+          <input type="hidden" name="sku" value="" />
+          <button type="submit">Buy Now</button>
+        </form>
+      </div>
+    </template>
+
+
+
       </Layout>
     )
   }
